ef017cf5d3f4539e9f54e1cc7cb130de6e4506e5
socratone <gim2origin@gmail.com>
2021-01-10 12:00:26 +0900

    css에서 {}안에 있을 때에만 key 색깔이 달라지도록 수정

--- src/helper/adoptCSSColor.js
+++ src/helper/adoptCSSColor.js
@@ -5,23 +5,30 @@ import { Green, Orange } from '../components/common/fontColor';
 const adoptCSSColor = text => {
   let texts = [];
   let tempText = '';
+  let isInBracket = false;
 
   for (let i = 0; i < text.length; i++) {
     tempText += text[i];
 
-    // :이 나오면 앞쪽 스트링 전부
-    if (text[i + 1] === ':') {
-      const keyWord = findKeyWord(tempText);
+    if (text[i] === '{') isInBracket = true;
+    else if (text[i] === '}') isInBracket = false;
 
-      if (keyWord.length > 0) {
-        const normalText = tempText.slice(0, -keyWord.length);
-        texts.push(normalText);
-        texts.push(<Orange key={i}>{keyWord}</Orange>);
-        tempText = '';
+    // {} 안에 있을 때만
+    if (isInBracket) {
+      // :이 나오면 앞쪽 스트링 전부
+      if (text[i + 1] === ':') {
+        const keyWord = findKeyWord(tempText);
+  
+        if (keyWord.length > 0) {
+          const normalText = tempText.slice(0, -keyWord.length);
+          texts.push(normalText);
+          texts.push(<Orange key={i}>{keyWord}</Orange>);
+          tempText = '';
+        }
+        continue;
       }
-      continue;
     }
-
+    
     // {가 나오면 앞쪽 스트링 전부
     if (text[i + 1] === '{') {
       const queryWord = findQueryWord(tempText);
