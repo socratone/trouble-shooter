342ce3b261320d78b9b5cf3400f1207af06bb525
socratone <gim2origin@gmail.com>
2021-01-20 18:37:04 +0900

    TODO 작성

--- src/helper/filterItemsByQuery.js
+++ src/helper/filterItemsByQuery.js
@@ -1,7 +1,7 @@
 const filterItemsByQuery = ({ items , query }) => {
   return items.filter(item => {
     if (item.title.toLowerCase().indexOf(query) >= 0) return true;
-    // TODO: 본문 검색
+    // TODO: 본문 검색을 위해서는 새로운 api가 필요하다.
   });
 };
 
