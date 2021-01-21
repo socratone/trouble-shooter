5bd30b51b477bb3b401ed8587193236ff37fc208
socratone <gim2origin@gmail.com>
2021-01-22 05:38:15 +0900

    이메일, 전화번호 정보 표시

--- src/components/About/Header.js
+++ src/components/About/Header.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PhoneIcon from '../icon/PhoneIcon';
 import EmailIcon from '../icon/EmailIcon';
 import GithubIcon from '../icon/GithubIcon';
 import BlogIcon from '../icon/BlogIcon';
 import styles from './Header.module.scss';
 
-const Hedaer = () => {
+let phoneNumberWidth = 0;
+let emailWidth = 0;
+const Header = () => {
+  const [phoneNumberSize, setPhoneNumberSize] = useState(0);
+  const [emailSize, setEmailSize] = useState(0);
+
+  const handlePhoneClick = () => {
+    if (phoneNumberWidth === 0) {
+      let timer = setInterval(() => {
+        phoneNumberWidth += 2;
+        setPhoneNumberSize(phoneNumberWidth)
+        if (phoneNumberWidth >= 120) clearInterval(timer);
+      }, 5);
+    } else {
+      let timer = setInterval(() => {
+        phoneNumberWidth -= 2;
+        setPhoneNumberSize(phoneNumberWidth)
+        if (phoneNumberWidth <= 0) clearInterval(timer);
+      }, 5);
+    }
+  };
+
+  const handleEmailClick = () => {
+    if (emailWidth === 0) {
+      let timer = setInterval(() => {
+        emailWidth += 2;
+        setEmailSize(emailWidth)
+        if (emailWidth >= 175) clearInterval(timer);
+      }, 5);
+    } else {
+      let timer = setInterval(() => {
+        emailWidth -= 2;
+        setEmailSize(emailWidth)
+        if (emailWidth <= 0) clearInterval(timer);
+      }, 5);
+    }
+  };
+
   return (  
     <header className={styles.header}>
       <h1 className={styles.title}>
@@ -15,14 +52,20 @@ const Hedaer = () => {
         코딩을 놀이 삼아 끊임없이 작업하고 콘텐츠를 만드는 개발자입니다.
       </p>
       <address className={styles.linkWrap}>
-        <a className={styles.link} href="https://github.com/socratone" target="_blank">
+        <a className={styles.link} onClick={handlePhoneClick}>
           <p><PhoneIcon size="30" color="whitesmoke" /></p>
           <p>Phone</p>
         </a>
-        <a className={styles.link} href="https://github.com/socratone" target="_blank">
+        <p className={styles.contact} style={{ width: phoneNumberSize + 'px' }}>
+          010-2569-1521
+        </p>
+        <a className={styles.link} onClick={handleEmailClick}>
           <p><EmailIcon size="30" color="whitesmoke" /></p>
           <p>Email</p>
         </a>
+        <p className={styles.contact} style={{ width: emailSize + 'px' }}>
+          gim2origin@gmail.com
+        </p>
         <a className={styles.link} href="https://github.com/socratone" target="_blank">
           <p><GithubIcon size="30" color="whitesmoke" /></p>
           <p>Github</p>
@@ -46,4 +89,4 @@ const Hedaer = () => {
   );
 }
  
-export default Hedaer;
+export default Header;
