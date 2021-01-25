156fc243bddda511d850f9a45ac626e91558a80b
socratone <gim2origin@gmail.com>
2021-01-25 12:37:18 +0900

    더보기 버튼 추가

--- src/components/About/Skill.js
+++ src/components/About/Skill.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { frontEndStack, backEndStack, deployStack } from '../../data/skill';
 import styles from './Skill.module.scss';
 import Stack from './Stack';
@@ -9,10 +9,17 @@ import WriteIcon from '../icon/WriteIcon';
 import SectionHeader from './SectionHeader';
 
 const Skill = () => {
+  const [isDetail, setDetail] = useState(false);
+
+  const handleDetailButton = () => {
+    if (isDetail) setDetail(false);
+    else setDetail(true);
+  };
+
   return (  
     <section className={styles.wrap}>
       <SectionHeader title="SKILL">
-        사용 가능한 기술 Stack은 다음과 같습니다.
+        사용 가능한 기술 Stack은 아래와 같습니다.
       </SectionHeader>
       <div className={styles.extraBackground} />
       <NormalPageFrame>
@@ -24,14 +31,23 @@ const Skill = () => {
               </div>
             </div>
             <h3 className={styles.itemTitle}>Front-end</h3>
-            <h4 className={styles.itemSubTitle}>능숙한 Stack</h4>
+            <h4 className={styles.itemSubTitle}>Skillful Stack</h4>
             {frontEndStack.skillfuls.map(stack => 
               <Stack key={stack.id} item={stack}>{stack.value}</Stack>
             )}
-            <h4 className={styles.itemSubTitle}>경험해본 Stack</h4>
-            {frontEndStack.experienceds.map(stack => 
-              <Stack key={stack.id} item={stack}>{stack.value}</Stack>
-            )}
+            {isDetail && <>
+              <h4 className={styles.itemSubTitle}>사용해본 Stack</h4>
+              {frontEndStack.experienceds.map(stack => 
+                <Stack key={stack.id} item={stack}>{stack.value}</Stack>
+              )}
+            </>}
+            <li className={styles.buttonWrap}>
+              <button 
+                className={styles.viewButton} 
+                onClick={handleDetailButton}>
+                  {isDetail ? '숨기기' : '더보기'}
+              </button>
+            </li>
           </article>
           <article className={styles.gridItem}>
             <div className={styles.itemIcon}>
@@ -40,14 +56,23 @@ const Skill = () => {
               </div>
             </div>
             <h3 className={styles.itemTitle}>Back-end</h3>
-            <h4 className={styles.itemSubTitle}>능숙한 Stack</h4>
+            <h4 className={styles.itemSubTitle}>Skillful Stack</h4>
             {backEndStack.skillfuls.map(stack => 
               <Stack key={stack.id} item={stack}>{stack.value}</Stack>
             )}
-            <h4 className={styles.itemSubTitle}>경험해본 Stack</h4>
-            {backEndStack.experienceds.map(stack => 
-              <Stack key={stack.id} item={stack}>{stack.value}</Stack>
-            )}
+            {isDetail && <>
+              <h4 className={styles.itemSubTitle}>사용해본 Stack</h4>
+              {backEndStack.experienceds.map(stack => 
+                <Stack key={stack.id} item={stack}>{stack.value}</Stack>
+              )}
+            </>}
+            <li className={styles.buttonWrap}>
+              <button 
+                className={styles.viewButton} 
+                onClick={handleDetailButton}>
+                  {isDetail ? '숨기기' : '더보기'}
+              </button>
+            </li>
           </article>
           <article className={styles.gridItem}>
             <div className={styles.itemIcon}>
@@ -56,14 +81,23 @@ const Skill = () => {
               </div>
             </div>
             <h3 className={styles.itemTitle}>Deployment</h3>
-            <h4 className={styles.itemSubTitle}>능숙한 Stack</h4>
+            <h4 className={styles.itemSubTitle}>Skillful Stack</h4>
             {deployStack.skillfuls.map(stack => 
               <Stack key={stack.id} item={stack}>{stack.value}</Stack>
             )}
-            <h4 className={styles.itemSubTitle}>경험해본 Stack</h4>
-            {deployStack.experienceds.map(stack => 
-              <Stack key={stack.id} item={stack}>{stack.value}</Stack>
-            )}
+            {isDetail && <>
+              <h4 className={styles.itemSubTitle}>사용해본 Stack</h4>
+              {deployStack.experienceds.map(stack => 
+                <Stack key={stack.id} item={stack}>{stack.value}</Stack>
+              )}
+            </>}
+            <li className={styles.buttonWrap}>
+              <button 
+                className={styles.viewButton} 
+                onClick={handleDetailButton}>
+                  {isDetail ? '숨기기' : '더보기'}
+              </button>
+            </li>
           </article>
         </section>
       </NormalPageFrame>
