2d06b5d0b7bf00bf5ea89f6183cf864a7123093d
socratone <gim2origin@gmail.com>
2021-01-25 11:48:29 +0900

    About Introduce 문구 수정

--- src/components/About/Introduce.js
+++ src/components/About/Introduce.js
@@ -5,8 +5,18 @@ const Introduce = () => {
   return (  
     <section>
       <section className={styles.introduce}>
-        <h2 className={styles.title}>안녕하세요! Front-end 개발자 김기원입니다.</h2>
-        <p className={styles.text}>콘텐츠 만드는 일을 좋아해 글쓰기부터 작곡까지 다양한 일을 했습니다. 이러한 경험을 바탕으로 디자인에 대한 감각이 있고 콘텐츠를 보여주는 웹 페이지를 어떻게 구성하면 좋을지 쉽게 파악할 수 있습니다.</p>
+        <h2 className={styles.title}>
+          <span>안녕하세요!</span>
+          <span>개발자 김기원입니다.</span>
+          <span>방문해주셔서 감사합니다.</span>
+        </h2>
+        <p className={styles.text}>
+          저는 대중음악 작곡, 모션 그래픽을 이용한 영상, 블로그 등 다양한 콘텐츠를 만들었던 경험이 있습니다. 
+          이를 바탕으로 특정 사이트에 접속한 사용자가 무엇을 바라는지 쉽게 파악하고
+          사용자에게 더 나은 UI와 UX를 제공할 수 있습니다.
+          또한 정리하는 습관 때문에 문제를 해결하면서 알게 된 새로운 정보들을 블로그에 기록하고
+          한 번 해결했던 문제가 다시 나왔을 때 빠르고 정확하게 처리합니다.
+        </p>
       </section>
     </section>
   );
