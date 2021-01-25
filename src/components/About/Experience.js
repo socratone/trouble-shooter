e5694fe4a9e5272d9d47a92368f217325755fd3c
socratone <gim2origin@gmail.com>
2021-01-25 21:50:43 +0900

    Experience 이미지 수정

--- src/components/About/Experience.js
+++ src/components/About/Experience.js
@@ -7,17 +7,18 @@ import styles from './Experience.module.scss';
 const Experience = () => {
   return (  
     <section className={styles.wrap}>
-      <SectionHeader title="개발 경력">
+      <SectionHeader title="Developer Experience">
+        개발 경력은 다음과 같습니다.
       </SectionHeader>
       <NormalPageFrame>
         <div className={styles.itemWrap}>
           <Company
-            image="https://image.rocketpunch.com/company/250/iportfolio_logo_1547515990.png?s=400x400&t=inside" 
+            image="https://trouble-shooter.s3.ap-northeast-2.amazonaws.com/profile/iportfolio.png" 
             name="아이포트폴리오"
             position="Front-end 인턴"
             term="2020년 9월 - 11월" />
           <Company
-            image="https://image.rocketpunch.com/company/19718/code-states_logo_1604475831.png?s=400x400&t=inside" 
+            image="https://trouble-shooter.s3.ap-northeast-2.amazonaws.com/profile/code-states.png" 
             name="코드스테이츠"
             position="수강생"
             term="2020년 1월 - 7월" />
