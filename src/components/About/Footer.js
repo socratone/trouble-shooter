790da8b775a55eebe1e3232d1e89bd231634adf6
socratone <gim2origin@gmail.com>
2021-01-27 08:10:21 +0900

    Footer 수정

--- src/components/About/Footer.js
+++ src/components/About/Footer.js
@@ -5,6 +5,9 @@ import styles from './Footer.module.scss';
 const Footer = () => {
   return (  
     <footer className={styles.footer}>
+      <p className={styles.copyright}>
+        © 2021 Socratone
+      </p>
     </footer>
   );
 }
