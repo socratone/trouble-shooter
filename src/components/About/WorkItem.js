87acfce82de023fdfd769e21b367e1e419c26478
socratone <gim2origin@gmail.com>
2021-02-02 03:33:44 +0900

    RecentProject의 detail 경계선 추가

--- src/components/About/WorkItem.js
+++ src/components/About/WorkItem.js
@@ -32,46 +32,50 @@ const WorkItem = ({
   };
 
   return (  
-    <article className={styles.itemWrap} style={setBackgroundColor()}>
-      <div className={styles.item} style={setPadding()}>
-        <div className={styles.imageArea} style={setOrder()}>
-          <img src={image} alt={title} className={styles.image}/>
+    <>
+      <article className={styles.itemWrap} style={setBackgroundColor()}>
+        <div className={styles.item} style={setPadding()}>
+          <div className={styles.imageArea} style={setOrder()}>
+            <img src={image} alt={title} className={styles.image}/>
+          </div>
+          <div>
+            <p className={styles.subTitle}>{subTitle}</p>
+            <p className={styles.title}>{title}</p>
+            <p className={styles.term}>{term}</p>
+            <div className={styles.description}>{description}</div>
+            <button 
+              className={styles.firstButton}
+              style={{ boxShadow: setBoxShadow() }}
+              onClick={handleViewDetail}>
+                <p>{isViewDetail ? '접기' : '펼치기'}</p>
+            </button>
+            {link && <a 
+              className={styles.secondButton}
+              href={link}
+              rel="noreferrer"
+              target="_blank">
+                <div>
+                  <p>홈페이지</p>
+                </div>
+            </a>}
+            {mac && <a 
+              className={styles.secondButton}
+              href={mac}
+              download>
+                <div>
+                  <p className={styles.icon}><AppleIcon size="16" /></p>
+                  <p className={styles.marginText}>다운로드</p>
+                </div>
+            </a>}
+          </div>
         </div>
-        <div>
-          <p className={styles.subTitle}>{subTitle}</p>
-          <p className={styles.title}>{title}</p>
-          <p className={styles.term}>{term}</p>
-          <div className={styles.description}>{description}</div>
-          <button 
-            className={styles.firstButton}
-            style={{ boxShadow: setBoxShadow() }}
-            onClick={handleViewDetail}>
-              <p>{isViewDetail ? '접기' : '펼치기'}</p>
-          </button>
-          {link && <a 
-            className={styles.secondButton}
-            href={link}
-            rel="noreferrer"
-            target="_blank">
-              <div>
-                <p>홈페이지</p>
-              </div>
-          </a>}
-          {mac && <a 
-            className={styles.secondButton}
-            href={mac}
-            download>
-              <div>
-                <p className={styles.icon}><AppleIcon size="16" /></p>
-                <p className={styles.marginText}>다운로드</p>
-              </div>
-          </a>}
-        </div>
+      </article>
+      <article className={styles.itemWrap} style={setBackgroundColor()}>
         {isViewDetail && <div className={styles.detail}>
           {children}
         </div>}
-      </div>
-    </article>
+      </article>
+    </>
   );
 }
  
