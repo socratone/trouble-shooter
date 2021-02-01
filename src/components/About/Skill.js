1d9227e7af57b9b9a8bb081a7225a783219f4d76
socratone <gim2origin@gmail.com>
2021-02-01 18:14:07 +0900

    Skill 내용 수정

--- src/components/About/Skill.js
+++ src/components/About/Skill.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { frontEndStacks, backEndStacks } from '../../data/skill';
-import Stack from './Stack';
 import NormalPageFrame from '../common/NormalPageFrame';
 import GearIcon from '../icon/GearIcon';
 import WriteIcon from '../icon/WriteIcon';
@@ -8,14 +6,6 @@ import SectionHeader from './SectionHeader';
 import styles from './Skill.module.scss';
 
 const Skill = () => {
-  const setStack = (item, i) => {
-    if (item.type === 'category') {
-      return <h4 className={styles.itemSubTitle}>{item.value}</h4>
-    } else if (item.type === 'stack') {
-      return <Stack key={i}>{item.value}</Stack>
-    }
-  }
-
   return (  
     <section className={styles.wrap}>
       <SectionHeader title="SKILL">
@@ -25,25 +15,62 @@ const Skill = () => {
       <NormalPageFrame>
         <main className={styles.main}>
           <article className={styles.gridItem}>
-            <div>
-              <div className={styles.itemIcon}>
-                <div className={styles.writeIcon}>
-                  <WriteIcon size="60" color="#fdc073" />
-                </div>
-              </div>
-              <h3 className={styles.itemTitle}>Front-end</h3>
-              {frontEndStacks.map((item, i) => setStack(item, i))}
+            <div className={styles.itemIcon}>
+              <WriteIcon size="60" color="#fdc073" />
+            </div>
+            <h3 className={styles.title}>Front-end</h3>
+            <div className={styles.stackWrap}>
+              <p className={styles.category}>Language</p>
+              <p className={styles.stack}>JavaScript</p>
+              <p className={styles.stack}>TypeScript</p>
+            </div>
+            <div className={styles.stackWrap}>
+              <p className={styles.category}>Framework</p>
+              <p className={styles.stack}>React</p>
+              <p className={styles.stack}>Electron</p>
+            </div>
+            <div className={styles.stackWrap}>
+              <p className={styles.category}>State Management</p>
+              <p className={styles.stack}>Redux</p>
+            </div>
+            <div className={styles.stackWrap}>
+              <p className={styles.category}>Style</p>
+              <p className={styles.stack}>SASS</p>
+              <p className={styles.stack}>Styled Component</p>
+            </div>
+            <div className={styles.stackWrap}>
+              <p className={styles.category}>Bundler</p>
+              <p className={styles.stack}>Webpack</p>
+            </div>
+            <div className={styles.stackWrap}>
+              <p className={styles.category}>배포</p>
+              <p className={styles.stack}>S3</p>
+              <p className={styles.stack}>Route 53</p>
             </div>
           </article>
           <article className={styles.gridItem}>
-            <div>
-              <div className={styles.itemIcon}>
-                <div className={styles.gearIcon}>
-                  <GearIcon size="60" color="#fdc073" />
-                </div>
-              </div>
-              <h3 className={styles.itemTitle}>Back-end</h3>
-              {backEndStacks.map((item, i) => setStack(item, i))}
+            <div className={styles.itemIcon}>
+              <GearIcon size="60" color="#fdc073" />
+            </div>
+            <h3 className={styles.title}>Back-end</h3>
+            <div className={styles.stackWrap}>
+              <p className={styles.category}>Server</p>
+              <p className={styles.stack}>NodeJS</p>
+            </div>
+            <div className={styles.stackWrap}>
+              <p className={styles.category}>Database</p>
+              <p className={styles.stack}>MySQL</p>
+            </div>
+            <div className={styles.stackWrap}>
+              <p className={styles.category}>인증 / 보안</p>
+              <p className={styles.stack}>bcrypt</p>
+              <p className={styles.stack}>JWT</p>
+            </div>
+            <div className={styles.stackWrap}>
+              <p className={styles.category}>배포</p>
+              <p className={styles.stack}>EC2</p>
+              <p className={styles.stack}>RDS</p>
+              <p className={styles.stack}>Docker</p>
             </div>
           </article>
         </main>
