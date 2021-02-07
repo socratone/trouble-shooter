613683839fb386c6e7ea41055bb76c7f43b5389a
socratone <gim2origin@gmail.com>
2021-02-08 01:30:56 +0900

    admin api에 axios 적용

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,23 +1,20 @@
+const axios = require('axios');
 const url = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 
 const postSignin = async password => {
-  const headers = new Headers();
-  headers.append('Content-Type', 'application/json');
-
   try {
-    const raw = JSON.stringify({ password });
-
-    const res = await fetch(url + '/api/admin/', {
-      method: 'POST',
-      headers,
-      body: raw,
-      redirect: 'follow'
-    });
-
-    const result = await res.json();
+    const res = await axios.post(url + '/api/admin/', { password });
+    const result = res.data;
     return result;
-  } catch (error) {
-    return { error };
+  } catch (ex) {
+    if (
+      ex.response && 
+      ex.response.status === 400 ||
+      ex.response.status === 500
+    ) {
+      return { error: ex.response.data.error };
+    }
+    return { error: { message: '알 수 없는 에러가 발생했습니다.' } }
   }
 };
 
